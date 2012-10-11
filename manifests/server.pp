5b13b3f12fd205e6fbbebd3307defc0513271252
Adrien Thebo <adrien@puppetlabs.com>
2012-10-11 14:19:01 -0700

    Add mcollective 1.9 symlink hackery

--- manifests/server.pp
+++ manifests/server.pp
@@ -49,4 +49,20 @@ class mcollective::server (
 
   include mcollective::server::defaultplugins
   include mcollective::server::plugindir
+
+  # Mcollective packages currently install into ruby/1.8 instead of vendor_ruby
+  # for compatibility with hardy. If the current rubyversion is 1.9 then we
+  # need symlinks so mcollective can find itself.
+  if $rubyversion =~ /^1\.9/ {
+    file {
+      '/usr/lib/ruby/vendor_ruby/mcollective.rb':
+        ensure => link,
+        target => '/usr/lib/ruby/1.8/mcollective.rb',
+        before => Service['mcollective'];
+      '/usr/lib/ruby/vendor_ruby/mcollective':
+        ensure => link,
+        target => '/usr/lib/ruby/1.8/mcollective',
+        before => Service['mcollective'];
+    }
+  }
 }
