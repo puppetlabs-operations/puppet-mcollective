29e4bc78481e5d2808c0b3a56730a0c2b109bb2a
Zach Leslie <zach@puppetlabs.com>
2012-10-30 16:20:31 -0700

    update paramters for freebsd

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,20 @@
 class mcollective::params {
 
   $topicprefix = '/topic/'
-  $sharedir    = '/usr/share/mcollective'
+
+  case $kernel {
+    'freebsd': {
+      $sharedir    = '/usr/local/share/mcollective'
+      $configdir   = '/usr/local/etc/mcollective'
+      $servicename = 'mcollectived'
+    }
+    default: {
+      $sharedir    = '/usr/share/mcollective'
+      $configdir   = '/etc/mcollective'
+      $servicename = 'mcollective'
+    }
+  }
+
   $libdir      = "${sharedir}/plugins"
   $logfile     = '/var/log/mcollective.log'
 
