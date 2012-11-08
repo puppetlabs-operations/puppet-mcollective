2831beecd3d86d81c438016e718e62246c9aa212
Adrien Thebo <adrien@puppetlabs.com>
2012-11-08 13:49:27 -0800

    Remove dead values from mcollective::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,8 @@
-class mcollective::params {
-
-  $topicprefix = '/topic/'
+class mcollective::params(
+  $topicprefix = '/topic/',
+  $logfile     = '/var/log/mcollective.log',
+  $loglevel    = 'warn',
+) {
 
   case $kernel {
     'freebsd': {
@@ -16,16 +18,4 @@ class mcollective::params {
   }
 
   $libdir      = "${sharedir}/plugins"
-  $logfile     = '/var/log/mcollective.log'
-
-  $loglevel = hiera('mcollective_loglevel', 'warn')
-
-  $host = hiera('mcollective_host')
-  # port 61613 is more standard for the stomp protocol
-  $port = hiera('mcollective_port', '61614')
-
-  $user     = hiera('mcollective_user')
-  $password = hiera('mcollective_password')
-
-  $psk = hiera('mcollective_psk')
 }
