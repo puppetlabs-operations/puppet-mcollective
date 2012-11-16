b9fb2d88c23c8be6ab6cf3cfa257cabd4aaee927
Adrien Thebo <adrien@puppetlabs.com>
2012-11-16 14:20:52 -0800

    Add examples to mcollective::plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -21,6 +21,31 @@
 #
 # * default: mcollective
 #
+# == Examples
+#
+#
+#    # Install the `puppetd` and `service` applications. This would normally
+#    # only be applied to clients.
+#    mcollective::plugin {'application/puppetd': }
+#    mcollective::plugin {'application/service': }
+#
+#    # Install the `registration` agent to monitor mcollective server
+#    # connectivity. This would probably be installed on a monitor host.
+#    mcollective::plugin {'agent/registration':   has_ddl => false }
+#
+#    # Install a set of common mcollective agents that are useful on most
+#    # nodes. This will also install the DDL for these agent plugins
+#    # everywhere.
+#    mcollective::plugin {'agent/puppetd':   has_ddl => true }
+#    mcollective::plugin {'agent/puppetca':  has_ddl => true }
+#    mcollective::plugin {'agent/puppetral': has_ddl => true }
+#    mcollective::plugin {'agent/service':   has_ddl => true }
+#    mcollective::plugin {'agent/package':   has_ddl => true }
+#
+#    # Install a 3rd party agent plugin and DDL. The backing agent and ddl
+#    # files will be in `modules/puppet/files/plugins/agent/deploy.{rb,ddl}`
+#    mcollective::plugin {'agent/deploy': has_ddl => true, module => 'puppet' }
+#
 define mcollective::plugin(
   $has_ddl = false,
   $module = 'mcollective',
