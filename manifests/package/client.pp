ebdcf0df43fb632edcb8f53d61b8f8cfa92c6c3e
Adrien Thebo <adrien@puppetlabs.com>
2012-11-16 14:25:51 -0800

    Add docs for mcollective::package::client

--- manifests/package/client.pp
+++ manifests/package/client.pp
@@ -1,3 +1,8 @@
+# = Class: mcollective::package::client
+#
+# This class will pull in the mcollective::package::stomp dependency and
+# install the mcollective client gem.
+#
 class mcollective::package::client {
 
   require mcollective::package::stomp
