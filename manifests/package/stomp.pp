c618d2fe0de4629e04a4d67f6ae44d1eda5bf8bb
Adrien Thebo <adrien@puppetlabs.com>
2012-11-16 14:29:23 -0800

    Add documentation for mcollective::package::stomp

--- manifests/package/stomp.pp
+++ manifests/package/stomp.pp
@@ -1,3 +1,34 @@
+# = Class: mcollective::package::stomp
+#
+# Install the stomp package for mcollective connectors
+#
+# This class will be brought in automatically and does not need to be directly
+# included. If you need to manually specify parameters, it's recommended that
+# the data binding in Puppet 3 is used to inject these values.
+#
+# == Parameters
+#
+# [*packagename*]
+#
+# The name of the mcollective package to install. Defaults to the platform
+# specific value determined in mcollective::params
+#
+# [*provider*]
+#
+# The package provider to use for installation. Defaults to the platform
+# specific provider determined in mcollective::params
+#
+# == Notes
+#
+# A number of mcollective connectors require at least mcollective 1.2.2, but
+# a sufficiently recent package is usually unavailable via standard package
+# repositories. Puppet Labs provides up to date packages for stomp at
+# apt.puppetlabs.com and yum.puppetlabs.com. These repositories can be
+# installed with the following optional modules:
+#
+#  * http://forge.puppetlabs.com/ploperations/puppetlabs_apt
+#  * http://forge.puppetlabs.com/stahnma/puppetlabs_yum
+#
 class mcollective::package::stomp(
   $packagename = undef,
   $provider    = undef
