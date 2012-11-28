cc3d6ee3d77bace198928c6a688fcf035eb69e45
Ashley Penney <apenney@edx.org>
2012-11-28 16:10:18 +0000

    Make sure we create the deeper directory structure.

--- manifests/server/directories.pp
+++ manifests/server/directories.pp
@@ -3,7 +3,7 @@ class mcollective::server::directories {
   include '::mcollective::params'
 
   exec { 'mco-create-dirs':
-    command => "mkdir -p ${mcollective::params::sharedir}",
+    command => "mkdir -p ${mcollective::params::core_libdir}",
     creates => $mcollective::params::sharedir,
   }
 
